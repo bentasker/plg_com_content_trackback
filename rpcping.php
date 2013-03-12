31b2b91ab81f7a59476b89c35c5d35f58260a8f6
bentasker <github@bentasker.co.uk>
2013-03-12 16:56:29 +0000

    UNTESTED, bare bones of plugin created

--- rpcping.php
+++ rpcping.php
@@ -14,89 +14,108 @@ defined('_JEXEC') or die('Restricted access');
 
 // Init the plugin
 jimport( 'joomla.plugin.plugin' );
-JLoader::register('K2Plugin',JPATH_ADMINISTRATOR.DS.'components'.DS.'com_k2'.DS.'lib'.DS.'k2plugin.php');
-JTable::addIncludePath(JPATH_COMPONENT.DS.'tables');
 
-
-
-class plgK2ItemOrdering extends K2Plugin {
+class plgContentrpcping extends JPlugin
 
 	// Some params
-	var $pluginName = 'itemordering';
-	var $pluginNameHumanReadable = 'K2 item ordering';
+	var $pluginName = 'rpcping';
+	var $pluginNameHumanReadable = 'Com_Content RPC Ping functionality';
 	var $params = null;
 	
-	function plgK2ItemOrdering(&$subject, $params) {
+	function __construct(&$subject, $params) {
 		parent::__construct($subject, $params);
 		
-		$plugin = & JPluginHelper::getPlugin('k2', $this->pluginName);
+		$plugin = & JPluginHelper::getPlugin('content', $this->pluginName);
 		
 		// This was deprecated in 2.5 and doesn't exist in 3
 		//$params = new JParameter($plugin->params);
-		$params = new JRegistry($plugin->params);
+		$this->params = new JRegistry($plugin->params);
+
+		$this->conf = JFactory::getConfig();
+		    if (method_exists($this->conf,'getValue')){
+		    $this->oldcachetime = $this->conf->getValue('config.cachetime');
+		    $this->setfn = 'setValue';
+		}else{
+		    $this->oldcachetime = $this->conf->get('config.cachetime')
+		    $this->setfn = 'set';
+		}
+
+		// Get the Cache object
+		$this->cache = JFactory::getCache('plg_rpcping', 'output');
+
+        	
+	}
+
+	// Send the Ping
+	function sendPing(){
+	  $services = array(
+		      "yahoo"=>"http://api.my.yahoo.com/RPC2",
+		      "google"=>'http://blogsearch.google.com/ping/RPC2',
+		      "technorati"=>"http://rpc.technorati.com/rpc/ping",
+		      "yandex"=>"http://blogs.yandex.ru/");
+		      
+
+	  $source = JUri::current();
+	  $target = JUri::current();
+
+	  $service = 'http://blogsearch.google.com/ping/RPC2';
+
+	  $request = xmlrpc_encode_request("pingback.ping", array($source, $target));
+	  $context = stream_context_create(array('http' => array(
+					    'method' => "POST",
+					    'header' => "Content-Type: text/xml",
+					    'content' => $request
+					     )));
+
+	  $file = file_get_contents($service, false, $context);
+	  $response = xmlrpc_decode($file);
+
+
+
+
+	}
+	
+
+
+	public function onContentPrepare($context, &$item, &$params, $page = 0)
+	{
+		// Don't run this plugin when the content is being indexed
+		if (($context == 'com_finder.indexer') || (!function_exists('xmlrpc_decode')))
+		{
+			return true;
+		}
+
+		// Otherwise see if the content has been marked as updated
+
+	      if ($upd = $this->cache->get('plg_rpcping_content'.$item->id) && ($upd == '1')) {
+		    if ($this->sendPing()){
+		    // If it was successful, prevent further pings
+		      $this->cache->remove('plg_rpcping_content'.$item->id);
+		    }
+	      }
+
+
+
+
 	}
 
 
-	function onAfterK2Save( & $item, $isNew) {
+	/** Runs after pressing Save
+	*
+	* We don't actually know what the URL is going to be, so we're going to cheat and put something in the cache for onContentPrepare to check for
+	*
+	*
+	*/
+	function onAfterSave( & $item, $isNew) {
+		   $setfn = $this->setfn;
+
+		  	// Set the cache time to 30 days 
+		$this->conf->$setfn('config.cachetime', 604800 );
+		$this->cache->setCaching( 1 );
 		
-		
-		if ($isNew) {
+		$this->cache->store("1", 'plg_rpcping_content'.$item->id);
 
-
-			  // If category specific settings have been made, obey!
-		      if(($this->params->get('applyTo') == 0) && (!in_array($item->catid,$this->params->get('applCategories')))){
-			return '';
-		      }
-
-
-		
-	  
-
-
-			$db = & JFactory::getDBO();
-			
-			$query = "SELECT MIN(ordering) FROM #__k2_items WHERE catid={$item->catid} AND trash=0 AND ordering > 0";
-			$db->setQuery($query);
-			
-			$minOrdering = $db->loadResult();
-			$item->ordering = $minOrdering -1;
-	
-			if ($item->featured == 1){
-			$forder = $this->params->get('forder');
-			$item->featured_ordering = $forder;
-			}
-
-
-			$item->store();
-			      // No idea why you wouldn't want this enabled, but I'll leave it as an option
-			if ($this->params->get('initItemIds')) {
-				//$query = "SELECT id FROM #__k2_items WHERE catid={$item->catid} AND trash=0 ORDER BY ordering ASC";
-				$query = "UPDATE #__k2_items SET ordering=ordering+1 WHERE catid={$item->catid} AND trash=0";
-
-				$db->setQuery($query);
-				$db->query();
-				
-				if ($item->featured == 1){
-				    $query = "UPDATE #__k2_items SET featured_ordering=featured_ordering+1 WHERE catid={$item->catid} AND featured_ordering > $forder AND featured=1";
-
-				    $db->setQuery($query);
-				    $db->query();
-				}
-
-
-				
-				//$itemIds = $db->loadResultArray();
-				
-				//$item = &JTable::getInstance('K2Item', 'Table');
-				
-				/*foreach ($itemIds as $key => $itemId) {
-					$item->load($itemId);
-					$item->ordering = $key+1;
-					$item->store();
-				}*/
-			}
-		}
-		
+		$this->conf->$setfn('config.cachetime', $this->oldcachetime );
 		return '';
 	}
 	
